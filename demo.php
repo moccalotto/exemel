a2908121073d605b182ff97f42531f883d8eb17e
Kim Hansen <moccalotto@gmail.com>
2016-06-22 08:44:48 +0200

    Better demo

--- demo.php
+++ demo.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'src/Xml.php';
+require 'vendor/autoload.php';
 
 function ensure($condition)
 {
@@ -12,11 +12,22 @@ function ensure($condition)
 $xml = new Moccalotto\Exemel\Xml(new SimpleXmlElement('<root/>'));
 // <root />
 
+
+/*
+ * Add attr="attr0" to the root element
+ */
 $xml->set('[attr]', 'attr0');
 ensure($xml->get('[attr]') == 'attr0');
 ensure($xml->root()['attr'] == 'attr0');
 // <root attr="attr0" />
 
+
+
+/*
+ * Add an element called <foo>
+ * Inside that, add an element called <bar>
+ * Set the contents of <bar> to "el1"
+ */
 $xml->set('foo/bar', 'el1');
 ensure($xml->get('foo/bar') == 'el1');
 ensure((string) $xml->root()->foo->bar == 'el1');
@@ -26,6 +37,11 @@ ensure((string) $xml->root()->foo->bar == 'el1');
 //   </foo>
 // </root>
 
+
+
+/*
+ * Add attr="attr1" to the newly added <bar> element
+ */
 $xml->set('foo/bar[ding]', 'attr1');
 ensure($xml->get('foo/bar[ding]') == 'attr1');
 ensure((string) $xml->root()->foo->bar['ding'] == 'attr1');
@@ -35,6 +51,12 @@ ensure((string) $xml->root()->foo->bar['ding'] == 'attr1');
 //   </foo>
 // </root>
 
+
+
+/*
+ * Add a new <foo> element to the root.
+ * Inside that, add a <bar> element with the contents "el2"
+ */
 $xml->set('foo[]/bar', 'el2');
 ensure($xml->get('foo[1]/bar') == 'el2');
 ensure((string) $xml->root()->foo[1]->bar == 'el2');
@@ -46,3 +68,5 @@ ensure((string) $xml->root()->foo[1]->bar == 'el2');
 //     <bar>el2</bar>
 //   </foo>
 // </root>
+
+echo $xml->indentedXml();
